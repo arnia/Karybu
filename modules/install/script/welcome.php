c4acf38f134721a8415fd9fe268ed70ddf039ede
Corina Udrescu <corina.udrescu@gmail.com>
2013-02-27 17:17:57 +0200

    GLCMS-94 Create the dataset for the new layout - Added support for widget pages.

--- modules/install/script/welcome.php
+++ modules/install/script/welcome.php
@@ -1,14 +1,17 @@
 <?php
 
-// ko/en/...
-
-
 /**
  * Class responsible for generating the menu and the pages
  * that will exist on a first time installation
  */
 class DatasetCreator {
 
+    /**
+     * Inserts a new menu in the database
+     *
+     * @param $title
+     * @return DatasetMenu|object
+     */
     public function createNewMenu($title) {
         $menu = new stdClass();
         $menu->site_srl = 0;
@@ -26,12 +29,16 @@ class DatasetCreator {
     }
 
     /**
+     * Inserts a new layout in the database
+     *
      * Currently supports just layouts with one menu
      * Can easily be updated to support different main and bottom menu
      * by adding an extra parameter
      *
+     * @param $layout_name
+     * @param $layout_title
      * @param DatasetMenu $main_menu
-     * @return mixed
+     * @return int
      */
     public function createNewLayout($layout_name, $layout_title, DatasetMenu $main_menu) {
         // create Layout
@@ -62,8 +69,16 @@ class DatasetCreator {
         return $layout_srl;
     }
 
-    public function createNewArticlePage($layout_srl, $browser_title, $mid, $article_page_title) {
-        // 1. Insert page module
+    /**
+     * Helper function to insert a page module in the database
+     *
+     * @param $layout_srl
+     * @param $browser_title
+     * @param $mid
+     * @param $page_type
+     * @return mixed
+     */
+    private function insertPageModule($layout_srl, $browser_title, $mid, $page_type) {
         $page_args = new stdClass();
         $page_args->layout_srl = $layout_srl;
         $page_args->browser_title = $browser_title;
@@ -71,42 +86,135 @@ class DatasetCreator {
         $page_args->mid = $mid;
         $page_args->module_category_srl = 0;
         $page_args->page_caching_interval = 0;
-        $page_args->page_type = 'ARTICLE';
+        $page_args->page_type = $page_type;
         $page_args->skin = 'default';
 
         $oModuleController = &getController('module');
         $output = $oModuleController->insertModule($page_args);
+        return $output;
+    }
 
-        if(!$output->toBool()) return $output;
-
-        // 2. Insert document associated with this page
-        $oTemplateHandler = &TemplateHandler::getInstance();
-        $oDocumentController = &getController('document');
-        $lang = Context::getLangType();
-
-        $module_srl = $output->get('module_srl');
-
+    /**
+     * Helper function to insert a document in the database
+     *
+     * @param $module_srl
+     * @param $document_title
+     * @param $content
+     * @return stdClass
+     */
+    private function insertDocument($module_srl, $document_title, $content) {
         $obj = new stdClass();
         $obj->module_srl = $module_srl;
-        Context::set('version', __ZBXE_VERSION__);
-        $obj->title = $article_page_title;
-        $obj->content = $oTemplateHandler->compile('./modules/install/script/welcome_content', $mid.'_'.$lang);
+        $obj->title = $document_title;
+        $obj->content = $content;
 
+        $oDocumentController = &getController('document');
         $output = $oDocumentController->insertDocument($obj);
         if(!$output->toBool()) return $output;
 
+        return $obj;
+    }
+
+    /**
+     * Inserts a new article page in the database
+     *
+     * The contents of the page are read from the welcome_content/ folder
+     *
+     * @param $layout_srl
+     * @param $browser_title
+     * @param $mid
+     * @param $article_page_title
+     * @return mixed
+     */
+    public function createNewArticlePage($layout_srl, $browser_title, $mid, $article_page_title) {
+        // 1. Insert page module
+        $output = $this->insertPageModule($layout_srl, $browser_title, $mid, 'ARTICLE');
+        if(!$output->toBool()) return $output;
+
+        // 2. Insert document associated with this page
+        $lang = Context::getLangType();
+        $module_srl = $output->get('module_srl');
+        Context::set('version', __ZBXE_VERSION__);
+        $oTemplateHandler = &TemplateHandler::getInstance();
+        $content = $oTemplateHandler->compile('./modules/install/script/welcome_content', $mid.'_'.$lang);
+
+        $output = $this->insertDocument($module_srl, $article_page_title, $content);
+
         // 3. Update page to point to the newly created document
-        $document_srl = $output->get('document_srl');
+        $document_srl = $output->document_srl;
 
         $oModuleModel = &getModel('module');
         $module_info = $oModuleModel->getModuleInfoByModuleSrl($module_srl);
         $module_info->document_srl = $document_srl;
+        $oModuleController = &getController('module');
         $output = $oModuleController->updateModule($module_info);
         if(!$output->toBool()) return $output;
 
         return $module_srl;
     }
 
+    /**
+     * Creates a new widget page
+     * Takes its contents from a file in welcome_content/
+     *
+     * An example of the expected structure of the file
+     * can be seen in widget_demo_en.html
+     *
+     * @param $layout_srl
+     * @param $browser_title
+     * @param $mid
+     * @return mixed
+     */
+    public function createNewWidgetPage($layout_srl, $browser_title, $mid) {
+        // 1. Insert page module
+        $output = $this->insertPageModule($layout_srl, $browser_title, $mid, 'WIDGET');
+        if(!$output->toBool()) return $output;
+
+        $module_srl = $output->get('module_srl');
+        $lang = Context::getLangType();
+
+        // 2.1. Get page content
+        $page_content = FileHandler::readFile('./modules/install/script/welcome_content/' . $mid . '_' . $lang . '.html');
+
+        // 2.2. See what widgets exist
+        preg_match_all('/{(.*)}/', $page_content, $matches);
+        $page_widgets = $matches[1];
+
+        // 2.3. Foreach widget, create an associated document and add its content
+        foreach($page_widgets as $widget_id) {
+            // 2.3.1. Get widget content
+            $pattern = "/<!--$widget_id(.*?)-->/msu";
+            preg_match($pattern, $page_content, $matches);
+            $meta_widget_content = $matches[0];
+            $widget_content = $matches[1];
+
+            // 2.3.2. Insert the document
+            $output = $this->insertDocument($module_srl, $widget_id, $widget_content);
+            $document_srl = $output->document_srl;
+
+            // 2.3.3. Update page content to remove the "meta" info regarding the widget's content
+            $page_content = str_replace($meta_widget_content, '', $page_content);
+
+            // 2.3.4. Put the document srl instead of the widget_id
+            $page_content = str_replace('{' . $widget_id . '}', $document_srl, $page_content);
+        }
+
+        // 3. Update page content
+        $oModuleModel = &getModel('module');
+        $module_info = $oModuleModel->getModuleInfoByModuleSrl($module_srl);
+        $module_info->content = $page_content;
+        $oModuleController = &getController('module');
+        $output = $oModuleController->updateModule($module_info);
+        if(!$output->toBool()) return $output;
+
+        return $module_srl;
+    }
+
+    /**
+     * Sets the default module of a website
+     *
+     * @param $module_srl
+     */
     public function setHomepage($module_srl) {
         $site_args = new stdClass();
         $site_args->site_srl = 0;
@@ -118,6 +226,10 @@ class DatasetCreator {
 
 }
 
+/**
+ * Represents a menu from a website
+ * Allows easily adding menu items and generating the corresponding XML file
+ */
 class DatasetMenu {
     private $menu_srl;
     private $title;
@@ -188,4 +300,3 @@ $documentation_page = $dataset_creator->createNewArticlePage($layout_srl, 'Docum
 
 // 4. Set homepage
 $dataset_creator->setHomepage($welcome_page);
-
