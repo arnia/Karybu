0bdbdb87ee97080c72220bb704013f838afff39d
Corina Udrescu <corina.udrescu@gmail.com>
2013-03-22 16:27:46 +0200

    Added monolog and stopwatch dependencies

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInit2332cecedca5ccd91e423b5292812705::getLoader();
+return ComposerAutoloaderInit87a4e530ebbd4822d9bb0c7f725300da::getLoader();
