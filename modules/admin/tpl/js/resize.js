57a54c1bb2aa5934fe247a13ca40f8f0dd49d269
Iulian Anghel <iulian.anghel@gmail.com>
2013-04-19 15:54:07 +0300

    [#47646157] Horizontal scroll, responsive graph, changed icons and some colors, dashboard swatches are links now, responsive text size in swatces . Set min-width at 320px.

--- modules/admin/tpl/js/resize.js
+++ modules/admin/tpl/js/resize.js
@@ -1,6 +1,12 @@
 function getDashboardContentHeight(){
-    return Math.floor(($(window).height() - 80))
+    if (parseInt($(window).width()) <= 767) {
+        return Math.floor(($(window).height() - 160))
+    }
+    else{
+        return Math.floor(($(window).height() - 80))
+        }
 }
+
 
 function dynamicSize() {
 
@@ -9,11 +15,71 @@ function dynamicSize() {
     });
 }
 
+function addStackedToNav() {
+    $('.content .nav.nav-tabs').each( function(){
+        $(this).addClass('nav-stacked');
+    });
+}
+
+function removeStackedFromNav() {
+    $('.content .nav.nav-tabs.nav-stacked').each( function(){
+        $(this).removeClass('nav-stacked');
+    });
+}
+
+function destroyScrollbar() {
+    $("#kNav").mCustomScrollbar("destroy");
+}
+
+function rebuildHorizontalScrollbar() {
+    $("#kNav").mCustomScrollbar({
+        horizontalScroll:true,
+        scrollButtons: {
+            enable: true
+        }
+        });
+}
+
+function rebuildVerticalScrollbar() {
+    $("#kNav").mCustomScrollbar({
+        scrollButtons: {
+            enable: true
+        }
+    });
+}
+
 jQuery(document).ready( function() {
     $ = jQuery;
     dynamicSize();
-})
 
-jQuery(window).bind('resize', function(){
+    //trigger special events for phone view
+    $(window).load(function(){
+        $("#chart").attr('width',$("#chart-container").width());
+        RGraph.Redraw();
+        if (parseInt($(window).width()) <= 480) {
+            addStackedToNav()
+        }
+    });
+
+    $(window).resize(function(){
+        $("#chart").attr('width',$("#chart-container").width());
+        RGraph.Redraw();
+        if (parseInt($(window).width()) <= 480) {
+            addStackedToNav();
+        }
+        else{
+            removeStackedFromNav()
+        }
+
+    });
+
+
+
+
+});
+
+
+jQuery(window).bind('resize', function () {
     dynamicSize();
 });
+
