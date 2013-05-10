e829db16cb4d8e533285b7a6dd1a69c284972485
Marius Strajeru <marius.strajeru@gmail.com>
2013-05-10 18:00:39 +0300

    index.php refactoring

--- index.php
+++ index.php
@@ -1,17 +1,45 @@
 <?php
+use Symfony\Component\HttpFoundation\Request;
+use Karybu\HttpKernel\Kernel;
+use Karybu\Environment\Environment;
 require_once 'src/Karybu/Environment/Environment.php';
+//set default environment
+$envCode = Environment::DEFAULT_ENVIRONMENT;
+//check if environment file exists
 $filename = 'files/config/environment.txt';
 if (file_exists($filename)) {
-    $content = file_get_contents($filename);
-    $env = \Karybu\Environment\Environment::getEnvironment($content);
-    if (count($env) > 0) {
-        define('KARYBU_ENVIRONMENT', $content);
-    }
-    else {
-        define('KARYBU_ENVIRONMENT', \Karybu\Environment\Environment::DEFAULT_ENVIRONMENT);
-    }
+    $envCode = file_get_contents($filename);
 }
-else{
-    define('KARYBU_ENVIRONMENT', \Karybu\Environment\Environment::DEFAULT_ENVIRONMENT);
+//get the current valid environment
+$env = Environment::getEnvironment($envCode, true);
+define('KARYBU_ENVIRONMENT', $env['code']);
+if ($env['dev_mode']) {
+    /**
+     * dev protection
+     */
+    /*if (isset($_SERVER['HTTP_CLIENT_IP']) || isset($_SERVER['HTTP_X_FORWARDED_FOR']) || !in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', 'fe80::1', '::1'))) {
+        header('HTTP/1.0 403 Forbidden');
+        exit('You are not allowed to access this resource. Check '.basename(__FILE__).' for more information.');
+    }*/
 }
-require "index_" . KARYBU_ENVIRONMENT . ".php";
+$isCommandLine = ( php_sapi_name() == 'cli' );
+/**
+ * Declare constants for generic use and for checking to avoid a direct call from the Web
+ **/
+define('__XE__',   true);
+define('__ZBXE__', true); // deprecated : __ZBXE__ will be removed. Use __XE__ instead.
+
+require dirname(__FILE__) . '/config/config.inc.php';
+
+$validCommandLineCall = $isCommandLine && isset($argv[1]) && filter_var($argv[1], FILTER_VALIDATE_URL);
+
+//create request using first call parameter if the script is called from the console with a valid url as first param
+$request = $validCommandLineCall ? Request::create($argv[1]) : Request::createFromGlobals();
+
+$kernel = new Kernel($env['code'], $env['dev_mode']);
+
+$response = $kernel->handle($request);
+$response->prepare($request);
+$response->send();
+
+$kernel->terminate($request, $response);
--- src/Karybu/Environment/Environment.php
+++ src/Karybu/Environment/Environment.php
@@ -11,19 +11,22 @@ final class Environment
     /**
      * get environment details
      * @param string $code
+     * @param bool $useDefault - if true and env does not exist returns default env
      * @access public
      * @return array()
      */
-    public static function getEnvironment($env = null)
+    public static function getEnvironment($env = null, $useDefault = false)
     {
         if (count(self::$_environments) == 0) {
             $dev                = array();
             $dev['code']        = self::DEV_ENVIRONMENT;
             $dev['lang_key']    = 'dev';
+            $dev['dev_mode']    = true;
 
             $prod               = array();
             $prod['code']       = self::PROD_ENVIRONMENT;
             $prod['lang_key']   = 'prod';
+            $prod['dev_mode']    = false;
 
             self::$_environments[self::DEV_ENVIRONMENT] = $dev;
             self::$_environments[self::PROD_ENVIRONMENT]= $prod;
@@ -33,6 +36,9 @@ final class Environment
         }
         if (isset(self::$_environments[$env])) {
             return self::$_environments[$env];
+        }
+        elseif($useDefault){
+            return self::$_environments[self::DEFAULT_ENVIRONMENT];
         }
         return array();
     }
