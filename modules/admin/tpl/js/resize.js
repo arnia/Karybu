2caa97bd56b8d1dc30a82c0c771046588dcf7341
Corina Udrescu <corina.udrescu@gmail.com>
2013-05-10 18:23:44 +0300

    [#49663455] Create classes of resolutions for responsive layout

--- modules/admin/tpl/js/resize.js
+++ modules/admin/tpl/js/resize.js
@@ -99,26 +99,6 @@
     }
 
     /**
-     * Admin navigation tabs
-     */
-    function kNavigationTabs() {}
-    kNavigationTabs.initialize = function() {
-        if (isSmallScreen()) {
-            kNavigationTabs.stack();
-        }
-    }
-    kNavigationTabs.stack = function() {
-        $('.nav.nav-tabs').each( function(){
-            $(this).addClass('nav-stacked');
-        });
-    }
-    kNavigationTabs.unstack = function() {
-        $('.nav.nav-tabs.nav-stacked').each( function(){
-            $(this).removeClass('nav-stacked');
-        });
-    }
-
-    /**
      * Generic functions
      */
     function getDashboardContentHeight() {
@@ -151,10 +131,30 @@
         return parseInt($(window).width()) <= 767;
     }
 
+    function initializeScreenResolution()
+    {
+        var browserWidth = $(window).width();
+        var browserHeight = $(window).height();
+
+        $("body").removeClass("phone-screen tablet-screen desktop-screen large-desktop-screen");
+
+        if(browserWidth > 1200) {
+            $("body").addClass("large-desktop-screen");
+        } else if(768 <= browserWidth && browserWidth <= 1200) {
+            $("body").addClass("desktop-screen");
+        } else if(480 <= browserWidth && browserWidth <= 767) {
+            $("body").addClass("tablet-screen");
+        } else {
+            $("body").addClass("phone-screen");
+        }
+    }
+
     /**
      * Event handlers
      */
     $(window).load(function(){
+        initializeScreenResolution();
+
         kAdminContentArea.updateHeight();
         kAdminContentArea.initializeScrollbar();
 
@@ -162,23 +162,34 @@
         kActionsNav.initializeScrollbar();
 
         kDashboardGraph.initialize();
-        kNavigationTabs.initialize();
+
+        if($(".main-nav ul.nav").height() > 50) {
+            $(".main-nav").addClass("main-nav-stacked");
+        } else {
+            $(".main-nav").removeClass("main-nav-stacked");
+        }
 
         if (hasNav()){
             $('body').addClass('kBigHeader')
         }
 
         $(window).resize(function(){
+            initializeScreenResolution();
+
             kAdminContentArea.updateHeight();
             kDashboardGraph.redraw();
 
             if (isSmallScreen()) {
-                kNavigationTabs.stack();
                 kActionsNav.makeHorizontal();
             }
             else{
-                kNavigationTabs.unstack();
                 kActionsNav.makeVertical();
+            }
+
+            if($(".main-nav ul.nav").height() > 50) {
+                $(".main-nav").addClass("main-nav-stacked");
+            } else {
+                $(".main-nav").removeClass("main-nav-stacked");
             }
         });
     });
