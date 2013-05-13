e66bce0a97e15aed60d616f797045d7c3b1a5d12
Marius Strajeru <marius.strajeru@gmail.com>
2013-05-13 17:02:41 +0300

    [#48988691] - Update module notfication looks broken [#49759623] - admin buttons look dark on IE

--- modules/admin/admin.admin.view.php
+++ modules/admin/admin.admin.view.php
@@ -358,7 +358,8 @@
 				}
 			}
             Context::set('module_list', $module_list);
-            Context::set('needUpdate', $isUpdated);
+            //Context::set('needUpdate', $needUpdate);
+            Context::set('needUpdate', $needUpdate);
             Context::set('addTables', $addTables);
             Context::set('needUpdate', $needUpdate);
 
