5d6dc06e0fe7a1aead9dd97d4a7137c52159f2ce
Corina Udrescu <corina.udrescu@gmail.com>
2013-05-24 18:00:55 +0300

    [#50430619] Scrollable tables: fixed the tables widths to be 100% again

--- modules/admin/tpl/js/plugins.js
+++ modules/admin/tpl/js/plugins.js
@@ -3,6 +3,7 @@
     $.fn.scrollableTable = function() {
         return this.each(function() {
             $(this).wrap("<div class='horizontal-scroll'></div>");
+            $(this).css('width', $(this).width());
             $(this).parent().mCustomScrollbar({
                 horizontalScroll:true,
                 mouseWheel:false
