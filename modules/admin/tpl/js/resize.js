bb058057251ea3268be42692299792d7a2110339
Corina Udrescu <corina.udrescu@gmail.com>
2013-05-28 13:57:52 +0300

    [#50248051] Refactored scrollbar's code as jQuery plugins.

--- modules/admin/tpl/js/resize.js
+++ modules/admin/tpl/js/resize.js
@@ -9,137 +9,6 @@
      */
 
     /**
-     * Sitemap navigation (left sidebar)
-     */
-    function kSitemapNav() {}
-    kSitemapNav.initializeScrollbar = function() {
-        $("#kSidebar").mCustomScrollbar({
-            scrollInertia:300,
-            scrollButtons:{
-                enable:true
-            },
-            advanced:{
-                updateOnContentResize: true
-            }
-        });
-    }
-
-    /**
-     * Actions navigation (right sidebar)
-     */
-    function kActionsNav() {}
-    kActionsNav.initializeScrollbar = function() {
-        if(isSmallScreen() && isPortrait()) {
-            kActionsNav.initializeHorizontalScrollbar();
-        } else {
-            kActionsNav.initializeVerticalScrollbar();
-        }
-    }
-    kActionsNav.initializeVerticalScrollbar = function() {
-        $("#kNav").mCustomScrollbar({
-            scrollInertia:300,
-            scrollButtons:{
-                enable:true
-            },
-            advanced:{
-                updateOnContentResize: true
-            }
-        });
-
-        $("#kNav").mCustomScrollbar("scrollTo", ".active");
-    }
-    kActionsNav.initializeHorizontalScrollbar = function() {
-        $("#kNav").mCustomScrollbar({
-            scrollInertia:300,
-            horizontalScroll:true,
-            scrollButtons: {
-                enable: true
-            },
-            advanced:{
-                updateOnContentResize: true
-            }
-        });
-
-        $("#kNav").mCustomScrollbar("scrollTo", ".active");
-    }
-    kActionsNav.makeHorizontal = function() {
-        $("#kNav").mCustomScrollbar("destroy");
-        kActionsNav.initializeHorizontalScrollbar();
-    }
-    kActionsNav.makeVertical = function() {
-        $("#kNav").mCustomScrollbar("destroy");
-        kActionsNav.initializeVerticalScrollbar();
-    }
-
-    /**
-     * Admin content area
-     */
-    function kAdminContentArea() {}
-    kAdminContentArea.initializeScrollbar = function() {
-        $("#dashboard_content").mCustomScrollbar({
-            scrollButtons:{
-                enable:true
-            },
-            scrollInertia:300,
-            advanced:{
-                updateOnContentResize: true,
-                normalizeMouseWheelDelta: true,
-                autoScrollOnFocus: false
-            }
-        });
-    }
-    kAdminContentArea.updateHeight = function() {
-        $('#dashboard_content').css({
-            'height': getDashboardContentHeight()
-        });
-    }
-
-    /**
-     * Main navigation scrollbars
-     */
-    function kMainNav() {}
-    kMainNav.initializeScrollbar = function() {
-        if(!$("#kMainNav").length) return;
-
-        $("#kMainNav").mCustomScrollbar({
-            scrollInertia:300,
-            horizontalScroll:true,
-            autoHideScrollbar: true,
-            scrollButtons: {
-                enable: false
-            },
-            advanced:{
-                autoScrollOnFocus: true
-            }
-        });
-        $("#kMainNav").mCustomScrollbar("scrollTo", ".active");
-    }
-
-    /**
-     * Vertical scrollbars
-     */
-
-    function kVScroll() {}
-    kVScroll.initializeScrollbar = function() {
-        if(!$(".kVScroll").length) return;
-
-        $(".kVScroll").mCustomScrollbar({
-            scrollInertia:300,
-            horizontalScroll:true,
-            autoHideScrollbar: true,
-            scrollButtons: {
-                enable: false
-            },
-            advanced:{
-                autoScrollOnFocus: true
-            }
-        });
-        $(".kVScroll").mCustomScrollbar("scrollTo", ".active");
-    }
-
-
-
-    /**
      * Generic functions
      */
     function getDashboardContentHeight() {
@@ -156,9 +25,9 @@
             occupiedHeight += heightOfAdminMainNav;
         }
 
-        if(isSmallScreen() && isPortrait()) {
+        if($.isSmallScreen() && $.isPortrait()) {
             occupiedHeight += heightOfSidebarNavigationWhenMobile + heightOfActionsNavigationWhenMobile;
-        } else if(isSmallScreen() && !isPortrait()) {
+        } else if($.isSmallScreen() && !$.isPortrait()) {
             occupiedHeight += 0;
         } else {
             occupiedHeight += heightOfPageTitle;
@@ -171,18 +40,6 @@
     {
         return ($('.main-nav').length > 0);
     }
-
-
-    function isSmallScreen()
-    {
-        return parseInt($(window).width()) <= 767;
-    }
-
-    function isPortrait()
-    {
-        return $("body").hasClass("portrait");
-    }
-
 
     function initializeScreenResolution()
     {
@@ -215,30 +72,18 @@
      */
     $(window).load(function(){
         initializeScreenResolution();
-        kAdminContentArea.updateHeight();
-        kAdminContentArea.initializeScrollbar();
 
-        kSitemapNav.initializeScrollbar();
-        kActionsNav.initializeScrollbar();
+        $('#dashboard_content').verticalScrollbar({ updateHeight: getDashboardContentHeight});
+        $('#kSidebar').verticalScrollbar();
+        $('#kNav').smartScrollbar();
+        $('#kMainNav').horizontalScrollbar({ autoHideScrollbar: true });
 
         if (hasNav()){
             $('body').addClass('kBigHeader')
         }
 
-        kMainNav.initializeScrollbar();
-        kVScroll.initializeScrollbar();
-
         $(window).resize(function(){
             initializeScreenResolution();
-
-            kAdminContentArea.updateHeight();
-
-            if (isSmallScreen() && isPortrait()) {
-                kActionsNav.makeHorizontal();
-            }
-            else{
-                kActionsNav.makeVertical();
-            }
         });
     });
 
