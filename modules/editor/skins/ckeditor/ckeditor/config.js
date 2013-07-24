93dc8da6bd41297fca4f9f533a62f3929bd48c4d
dragan-dan <ddan.dragan@gmail.com>
2013-07-24 15:55:46 +0300

    added quote functionality to board

--- modules/editor/skins/ckeditor/ckeditor/config.js
+++ modules/editor/skins/ckeditor/ckeditor/config.js
@@ -2,8 +2,9 @@
  * @license Copyright (c) 2003-2013, CKSource - Frederico Knabben. All rights reserved.
  * For licensing, see LICENSE.html or http://ckeditor.com/license
  */
-
 CKEDITOR.editorConfig = function( config ) {
+    config.enterMode = CKEDITOR.ENTER_BR;
+    config.entities = 0;
 	// Define changes to default configuration here. For example:
 	// config.language = 'fr';
 	// config.uiColor = '#AADC6E';
