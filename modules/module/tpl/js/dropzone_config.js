209b1b1569c1a8899baf33a0150465e752ce32a4
dragan-dan <ddan.dragan@gmail.com>
2013-08-06 13:30:00 +0300

    allow multiple file upload with dropzone

--- modules/module/tpl/js/dropzone_config.js
+++ modules/module/tpl/js/dropzone_config.js
@@ -1,10 +1,14 @@
 jQuery(document).ready(function($) {
     Dropzone.options.fileboxUpload = {
         init: function() {
+            var uploaded = 0;
             this.on("success", function(file) {
-                //alert("Upl");
-                location.reload();
+                uploaded++;
+                if(this.files.length == uploaded){
+                    location.reload();
+                }
             });
+
         }
     };
 });
